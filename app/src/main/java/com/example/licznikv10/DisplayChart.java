fd7362504fbf3b7a4f4220979de7e29931f81584
Dastin Wilski <dastin.wilski@interia.pl>
2019-01-13 19:03:02 +0100

    This is final version tututu tututu.

--- app/src/main/java/com/example/licznikv10/DisplayChart.java
+++ app/src/main/java/com/example/licznikv10/DisplayChart.java
@@ -1,15 +1,31 @@
 package com.example.licznikv10;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.icu.util.RangeValueIterator;
+import android.net.wifi.WifiManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 import lecho.lib.hellocharts.model.Axis;
@@ -23,9 +39,10 @@ public class DisplayChart extends AppCompatActivity {
 
 
     LineChartView lineChartView;
+    Button onOff;
+    boolean flag = true;
 
-
-    /*Przykładowe dane do wykresu - chwilowo niepotrzebne i kłopotliwe przy dodawaniu danych z mainactivity
+/*Przykładowe dane do wykresu - chwilowo niepotrzebne i kłopotliwe przy dodawaniu danych z mainactivity
      String[] axisData = {};
     double[] yAxisData1 = {};
 
@@ -33,24 +50,31 @@ public class DisplayChart extends AppCompatActivity {
     */
 
     //Listy w których będą dane
-    //Wszystkie próbki
     static List yAxisValues = new ArrayList();
     static List axisValues = new ArrayList();
 
-    //Co trzecia próbka
     static List yAxisValues2 = new ArrayList();
     static List axisValues2 = new ArrayList();
 
+    static List yAxisValues3 = new ArrayList();
+    static List axisValues3 = new ArrayList();
 
-    //przesunąłem konstruktory, zeby inne funkcje(przyciski) miały dostęp do danych
+    static List yAxisValues4 = new ArrayList();
+    static List axisValues4 = new ArrayList();
+
     List lines = new ArrayList();
-    static LineChartData data = new LineChartData();
-
-    //Linia wykresu łącząca punkty okreslone przez dane z listy
     Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0")); //linia dla wszystkich próbek
     Line line2 = new Line(yAxisValues2).setColor(Color.RED); //linia dla co trzeciej próbki
+    Line line3 = new Line(yAxisValues3).setColor(Color.BLUE); //linia dla co trzeciej próbki
+    Line line4 = new Line(yAxisValues4).setColor(Color.GREEN); //linia dla co trzeciej próbki
 
     Axis axis = new Axis();
+
+
+
+    static LineChartData data = new LineChartData();
+    ScheduledExecutorService scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,8 +83,18 @@ public class DisplayChart extends AppCompatActivity {
         //Powiązanie z elementem chart(zadeklarowany w activity_display_chart.xml
         lineChartView = findViewById(R.id.chart);
 
+        onOff = findViewById(R.id.button2);
 
 
+
+
+
+
+
+
+
+
+                //Linia wykresu łącząca punkty okreslone przez dane z listy
 
 
 
@@ -77,19 +111,15 @@ public class DisplayChart extends AppCompatActivity {
 
 
         //Lista linii (chociaż my i tak będziemy mieć  raczej jedną)
-        //List lines = new ArrayList();
 
-
-        //dodanie podstwowej linii - wszystkie próbki
         lines.add(line);
-
 
         //Dodanie linii do wykresu
         //LineChartData data = new LineChartData();
         data.setLines(lines);
-        //data2.setLines(lines);
+
         //Ustawienia osi x (legenda)
-        //Axis axis = new Axis();
+
         axis.setName("Czas");
         axis.setValues(axisValues);
         axis.setMaxLabelChars(10);
@@ -98,8 +128,6 @@ public class DisplayChart extends AppCompatActivity {
         //axis.setHasTiltedLabels(true); // to robi, że wartości są pod kątem względem osi
         axis.setTextColor(Color.parseColor("#03A9F4"));
         data.setAxisXBottom(axis);
-
-
 
         //Ustawienia osi y (legenda)
         Axis yAxis = new Axis();
@@ -111,21 +139,39 @@ public class DisplayChart extends AppCompatActivity {
         //yAxis.setHasTiltedLabels(true);
         data.setAxisYLeft(yAxis);
 
-
-
-
-
         //Wyświetlenia wykresu
 
-        lineChartView.setLineChartData(data);
+
+
+
+            scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+                public void run() {
+                    if(flag)
+                    lineChartView.setLineChartData(data);
+                    else{}
+                }
+            }, 0, 1, TimeUnit.SECONDS);
+
+
 
     }
+public void OnOff(View view)
+{
+    flag = !flag;
+    if(flag)onOff.setText("STOP");
+    else onOff.setText("PLAY");
+}
+
+
+
+
+
 
     public void time1(View view)
     {
         axis.setMaxLabelChars(10);//ustawienie odstępu - potestować dla duzego zbioru danych
-                                    // w sumie może nie trzeba zmieniać, zależy jak dużo danych będzie  do narysowania
-                                    //zawsze można przyblizyc okreslony punkt i bedzie widac dokladna date
+        // w sumie może nie trzeba zmieniać, zależy jak dużo danych będzie  do narysowania
+        //zawsze można przyblizyc okreslony punkt i bedzie widac dokladna date
         lines.clear();  //czyszcenie zbioru linii
         lines.add(line);//dodanie do zbioru linii tej którą chcemy mieć
         lineChartView.setLineChartData(data);//odświezenia wykrsu
@@ -140,6 +186,22 @@ public class DisplayChart extends AppCompatActivity {
         lineChartView.setLineChartData(data);//odświezenia wykrsu
     }
 
+    public void time15(View view)
+    {
+        axis.setMaxLabelChars(4);//ustawienie odstępu - potestować dla duzego zbioru danych
+        lines.clear();//czyszcenie zbioru linii
+        lines.add(line3);//dodanie do zbioru linii tej którą chcemy mieć
 
+        lineChartView.setLineChartData(data);//odświezenia wykrsu
+    }
+
+    public void time60(View view)
+    {
+        axis.setMaxLabelChars(4);//ustawienie odstępu - potestować dla duzego zbioru danych
+        lines.clear();//czyszcenie zbioru linii
+        lines.add(line4);//dodanie do zbioru linii tej którą chcemy mieć
+
+        lineChartView.setLineChartData(data);//odświezenia wykrsu
+    }
 
 }
--- app/src/main/java/com/example/licznikv10/MainActivity.java
+++ app/src/main/java/com/example/licznikv10/MainActivity.java
@@ -12,13 +12,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import android.widget.TextView;
 
 import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.AxisValue;
@@ -36,61 +52,63 @@ public class MainActivity extends AppCompatActivity  {
     WifiP2pManager.Channel mChannel;
     BroadcastReceiver mReceiver;
     IntentFilter mIntent;
-    Button wifi, connect;
+    Button wykres, connect;
     EditText p2p;
-    int rozmiar = 0;    //liczba danych w pierwszym zbiorze
-    int rozmiar2 = 0;   //liczba danych w drugim zbiorze
+    int rozmiar = 0;
+    int rozmiar2 = 0;
+    int rozmiar3 = 0;
+    int rozmiar4 = 0;
+    float value = 0;
+    String IPn = "10.1.0.74";
     String currentDateTimeString;
+    String path;
+    private EditText radiation;
+    private EditText impulses;
+    private  EditText IPnum;
+    ArrayList<String> list = new ArrayList<String>();
+    ScheduledExecutorService scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
         initialize();
 
     }
 
     public void connect(View view) {
+        if (wifiManager.isWifiEnabled()) {
 
-    }
+            p2p.setText("wi-fi is on");
+            connect.setClickable(false);
+            scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+                public void run() {
+                    getWebsite();
+                }
+            }, 0, 1, TimeUnit.SECONDS);
 
-    public void wifiOnOff(View view) {
-       /* if (wifiManager.isWifiEnabled()) {
-            wifiManager.setWifiEnabled(false);
-            wifi.setText("Wi-fi off");
-            wifi.setTextColor(0xFFDD0A57);
         } else {
-            wifiManager.setWifiEnabled(true);
-            wifi.setText("Wi-fi on");
-            wifi.setTextColor(0xFF00574B);
-        }*/
 
 
-        /* Dodawaniae kolejnych liczb naturalnych w celach testowych,
-           docelowo gdy dane będą w postaci string czas, double pomiar będzie coś w stylu
-           DisplayChart.axisValues.add(rozmiar, new AxisValue(rozmiar).setLabel(czas));
-           DisplayChart.yAxisValues.add(rozmiar,new PointValue(rozmiar,pomiar));
-        */
-        currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-        DisplayChart.axisValues.add(rozmiar,new AxisValue(rozmiar).setLabel(currentDateTimeString));
-        DisplayChart.yAxisValues.add(rozmiar,new PointValue(rozmiar,rozmiar));
-        rozmiar = rozmiar + 1;
+            p2p.setText("turn on wi-fi");
+            connect.setClickable(true);
 
 
-        //zbieranie co trzeciej próbki do drugiego zbioru
-        if(rozmiar%3 == 0)
-        {
-            DisplayChart.axisValues2.add(rozmiar2,new AxisValue(rozmiar).setLabel(currentDateTimeString));
-            DisplayChart.yAxisValues2.add(rozmiar2,new PointValue(rozmiar2,rozmiar));
-            rozmiar2 = rozmiar2 + 1;
         }
-
-
     }
+
+
 
     public void initialize() {
-        wifi = findViewById(R.id.wifi);
+
         connect = findViewById(R.id.connect);
+        connect.setClickable(false);
+        radiation = findViewById(R.id.editText3);
+        impulses = findViewById(R.id.editText4);
+        IPnum = findViewById(R.id.editText2);
+        path = getApplicationContext().getFilesDir().getPath() + "/geiger1.txt";
+        IPnum.setText(IPn);
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
@@ -103,13 +121,29 @@ public class MainActivity extends AppCompatActivity  {
 
         p2p = findViewById(R.id.P2Pcheck);
         p2p.setVisibility(View.INVISIBLE);
+        wykres = findViewById(R.id.button);
+        wykres.setVisibility(View.INVISIBLE);
+        loadState();
+        wykres.setVisibility(View.VISIBLE);
         if (wifiManager.isWifiEnabled()) {
-            wifi.setText("Wi-fi on");
-            wifi.setTextColor(0xFF00574B);
+            p2p.setVisibility(View.VISIBLE);
+            p2p.setText("wi-fi is on");
+            connect.setClickable(false);
+            scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+                public void run() {
+                    getWebsite();
+                }
+            }, 0, 1, TimeUnit.SECONDS);
+
         } else {
-            wifi.setText("Wi-fi off");
-            wifi.setTextColor(0xFFDD0A57);
+
+            p2p.setVisibility(View.VISIBLE);
+            p2p.setText("turn on wi-fi");
+            connect.setClickable(true);
+
+
         }
+
 
     }
 
@@ -132,4 +166,184 @@ public class MainActivity extends AppCompatActivity  {
         super.onPause();
         unregisterReceiver(mReceiver);
     }
+
+
+    private void getWebsite() {
+
+
+            if (wifiManager.isWifiEnabled()) {
+
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final StringBuilder builder = new StringBuilder();
+                        final StringBuilder myBuilder = new StringBuilder();
+                        try{
+                        Document doc = Jsoup.connect("http://" + IPnum.getText()).get();
+
+                        Elements links = doc.getElementsByTag("p");
+
+
+                        for (Element link : links) {
+                            builder.append(link);
+                        }
+                        }catch (IOException e){}
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+
+
+                                    String myOne = builder.toString();
+                                    myOne = myOne.replace("<p>", "");
+                                    myOne = myOne.replace("LEV:", "x");
+                                    myOne = myOne.replace("IMP:", "");
+                                    myOne = myOne.replace("</p>", "");
+                                    myOne = myOne.replace("S:", "x");
+                                    myOne = myOne.replace("uS/h", "");
+                                    myOne = myOne.replace(" ", "");
+                                    String[] myOne2 = myOne.split("x");
+                                    String radiation1 = myOne2[2];
+                                    String impulses1 = myOne2[0];
+                                    radiation.setText(radiation1);
+                                    impulses.setText(impulses1);
+                                    float radiationf = Float.parseFloat(radiation1);
+                                    currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                                    DisplayChart.axisValues.add(rozmiar, new AxisValue(rozmiar).setLabel(currentDateTimeString));
+                                    DisplayChart.yAxisValues.add(rozmiar, new PointValue(rozmiar, radiationf));
+                                    rozmiar = rozmiar + 1;
+                                    if(rozmiar%5 == 0)
+                                    {
+                                        DisplayChart.axisValues2.add(rozmiar2,new AxisValue(rozmiar).setLabel(currentDateTimeString));
+                                        DisplayChart.yAxisValues2.add(rozmiar2,new PointValue(rozmiar2,radiationf));
+                                        rozmiar2 = rozmiar2 + 1;
+                                    }
+                                    if(rozmiar%15==0)
+                                    {
+                                        DisplayChart.axisValues3.add(rozmiar3,new AxisValue(rozmiar).setLabel(currentDateTimeString));
+                                        DisplayChart.yAxisValues3.add(rozmiar3,new PointValue(rozmiar3,radiationf));
+                                        rozmiar3 = rozmiar3 + 1;
+                                    }
+                                    if(rozmiar%60==0)
+                                    {
+                                        DisplayChart.axisValues4.add(rozmiar4,new AxisValue(rozmiar).setLabel(currentDateTimeString));
+                                        DisplayChart.yAxisValues4.add(rozmiar4,new PointValue(rozmiar4,radiationf));
+                                        rozmiar4 = rozmiar4 + 1;
+                                    }
+                                    myBuilder.append(radiation1).append("x").append(currentDateTimeString);
+                                    String temp = myBuilder.toString();
+                                    list.add(temp);
+                                    p2p.setText("OK");
+                                }catch (IndexOutOfBoundsException e)
+                                {
+                                    p2p.setText("connection error");
+                                }
+
+                            }
+                        });
+                    }
+                }).start();
+            } else {
+
+
+                p2p.setText("turn on wifi");
+            }
+
+    }
+    public void save(View view)
+    {
+        FileOutputStream outStream = null;
+        try {
+            File f = new File( path);
+            if(f.exists())
+            f.delete();
+            outStream = new FileOutputStream(f);
+            ObjectOutputStream objectOutStream = new ObjectOutputStream(outStream);
+
+
+            objectOutStream.writeObject(list);
+            objectOutStream.close();
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+    }
+    public void delete(View view)
+    {
+
+
+            File f = new File( path);
+            if(f.exists())
+            f.delete();
+
+
+
+
+
+    }
+
+    private void loadState()
+    {
+        ArrayList<String> s =null;
+        FileInputStream inStream = null;
+
+        try {
+            File f = new File( path);
+            inStream = new FileInputStream(f);
+            ObjectInputStream objectInStream = new ObjectInputStream(inStream);
+
+            s = ((ArrayList<String>) objectInStream.readObject());
+            objectInStream.close();
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        if(s!=null)
+
+        {
+            for(String x : s)
+            {
+                String myTemp[] = x.split("x");
+                DisplayChart.axisValues.add(rozmiar, new AxisValue(rozmiar).setLabel(myTemp[1]));
+                DisplayChart.yAxisValues.add(rozmiar, new PointValue(rozmiar, Float.parseFloat(myTemp[0])));
+                rozmiar = rozmiar + 1;
+
+                if(rozmiar%5 == 0)
+                {
+                    DisplayChart.axisValues2.add(rozmiar2,new AxisValue(rozmiar).setLabel(myTemp[1]));
+                    DisplayChart.yAxisValues2.add(rozmiar2,new PointValue(rozmiar2, Float.parseFloat(myTemp[0])));
+                    rozmiar2 = rozmiar2 + 1;
+                }
+                if(rozmiar%15==0)
+                {
+                    DisplayChart.axisValues3.add(rozmiar3,new AxisValue(rozmiar).setLabel(myTemp[1]));
+                    DisplayChart.yAxisValues3.add(rozmiar3,new PointValue(rozmiar3,Float.parseFloat(myTemp[0])));
+                    rozmiar3 = rozmiar3 + 1;
+                }
+                if(rozmiar%60==0)
+                {
+                    DisplayChart.axisValues4.add(rozmiar4,new AxisValue(rozmiar).setLabel(myTemp[1]));
+                    DisplayChart.yAxisValues4.add(rozmiar4,new PointValue(rozmiar4,Float.parseFloat(myTemp[0])));
+                    rozmiar4 = rozmiar4 + 1;
+                }
+
+
+                list.add(x);
+
+            }
+        }
+        else
+        {
+
+        }
+    }
 }
+
